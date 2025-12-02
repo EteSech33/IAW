97a7f5b8cbeb323434f48bae3439abedd54719d6
EteSech33 <fp_gabrielkirilov@ciudadjardin.org>
2025-12-02 12:45:47 +0100

    Ejercicio final de php, he cambiado nombre

--- ejercicio_final.php
+++ ejercicio_final.php
@@ -1,20 +1,29 @@
 <?php 
 session_start();
 // Crea la sesion "iniciar_sesion"
-$puntuacion=10;
+$puntuacion = 10;
+$suma = 0;
+$dado1 = 0;
+$dado2 = 0;
+$pa = 0;
+$vu = "";
 
-function tirar() {
-echo"estoy dentro de la funcion tirar";
+if (empty($_POST["vu"])){
+    $vu = "Debes de introducir un número.";
+}
+
+function tirar() { // Calcula los números de los dados
     $dado1 = rand(1,6);
 	$dado2 = rand(1,6);
 	$suma = $dado1 + $dado2;
 	return [$suma, $dado1, $dado2];
 }
 
-[$suma, $dado1, $dado2]= tirar();
+if (empty($dado1)) {
+    [$suma, $dado1, $dado2]= tirar();
+}
 
 function calcular_par($suma) {
-    echo"$suma dentro de calcluar par vale".$suma;
     if ($suma % 2 == 0) {
         return True;
     }
@@ -34,15 +43,18 @@ function comprobar_par($par) {
     }
 }
 
+
+
 ?>
 
 <!DOCTYPE html>
 <head>
-<title>Calculadora</title>
+<title>Dados</title>
 <link rel="stylesheet" href="../estilo/ejercicio1.css">
 </head>
 
 <body>
+    <h1>9. Ejercicio final: Funciones, Números Aleatorios y Control de Sesiones en PHP con Interacción del Usuario</h1>
     <?php if ($_SERVER["REQUEST_METHOD"] =! "POST"): // IF que escriba html, en vez de php?>
     
         <h1>ERROR</h1>
@@ -50,15 +62,24 @@ function comprobar_par($par) {
     <?php else: ?>
 
         <form action="<?= (htmlspecialchars($_SERVER["PHP_SELF"])); //El php se escribe en el propio documento.?>" method="post">
-            <p>pollo</p>
-            <input type="radio" name="paridad" value="Inpar">Inpar
-            <input type="radio" name="paridad" value="Par">Par
+            <p>Objetivo: <input type="text" name="vu" placeholder="Número"> <input type="submit" name="submit" value="Enviar"></p>
             <?php
-                echo "La suma es: $suma";
+                echo "<h3>Has lanzado los dados</h3>";
+                echo "<br>";
+                echo "DADO 1: $dado1";
+                echo "<br>";
+                echo "DADO 2: $dado2";
+                echo "<br>";           
+                echo "Suma total: $suma";
                 echo "<br>";
                 comprobar_par($par);
+                echo "<br>";
+                echo"$vu";
+                echo "<br>";
+                echo "Puntuación acumulada: $pa";
             ?>
         </form>
+        
 
     <?php endif; //Final del if?>
 </body>
