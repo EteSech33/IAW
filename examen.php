9bae39ffc4bb23a211528eca63c1bfe9ce3d9ab6
Chacho <fp_gabrielkirilov@ciudadjardin.org>
2025-12-15 21:16:34 +0100

    Examen final hecho bien con header

--- examen.php
+++ examen.php
@@ -1,32 +1,45 @@
 <?php
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST"): // IF que escriba html, en vez de php
 
-/*  __      __        _       _     _           
-    \ \    / /       (_)     | |   | |          
-     \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___ 
+/*  __      __        _       _     _
+    \ \    / /       (_)     | |   | |
+     \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___
       \ \/ / _` | '__| |/ _` | '_ \| |/ _ \/ __|
        \  / (_| | |  | | (_| | |_) | |  __/\__ \
         \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
 */
-$parirdad = null;
-$coger_paridad = null;
-$eleccion = null;
-$var_paridad = null;
-$suma = null;
-$comprobar = null;
-$valor_usuario = null;
-$reiniciar_juego = null;
-$error = null;
-
 /*
-    ______                 _                      
-    |  ____|              (_)                      
-    | |__ _   _ _ __   ___ _  ___  _ __   ___  ___ 
+   Si el array de $_SESSION['vista'] no existe,
+   entonces pone todo a null, haciendo que no haya errores
+   de que las variables no están definidas
+*/
+$vista = $_SESSION['vista'] ?? null;
+if ($vista) {
+    $valor_usuario = $vista['valor_usuario'];
+    $dado1 = $vista['dado1'];
+    $dado2 = $vista['dado2'];
+    $suma = $vista['suma'];
+    $mensaje_comprobar = $vista['mensaje_comprobar'];
+    $eleccion = $vista['eleccion'];
+    $coger_paridad = $vista['paridad'];
+    $var_paridad = $vista['var_paridad'];
+
+}
+$error=null;
+$reiniciar_juego = null;
+$var_paridad = null;
+$comprobar_juego = false;
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){ // IF que escriba html, en vez de php
+/*
+    ______                 _
+    |  ____|              (_)
+    | |__ _   _ _ __   ___ _  ___  _ __   ___  ___
     |  __| | | | '_ \ / __| |/ _ \| '_ \ / _ \/ __|
     | |  | |_| | | | | (__| | (_) | | | |  __/\__ \
     |_|   \__,_|_| |_|\___|_|\___/|_| |_|\___||___/
-                                               
+
 */
 // Calcular si la suma es par o impar
 function calcular_par($suma) {
@@ -48,22 +61,22 @@ function tirar() { // Calcula los números de los dados
 // Comprueba que el numero $suma sea par o impar
 function imprimir_paridad($paridad){
     if ($paridad) {
-        $comprobar = "El número es Par.";
-        return $comprobar;
+        $mensaje_comprobar = "El número es Par.";
+        return $mensaje_comprobar;
     }
     else {
-        $comprobar = "El número es Impar.";
-        return $comprobar;
+        $mensaje_comprobar = "El número es Impar.";
+        return $mensaje_comprobar;
     }
 }
 
 /*
-      _ _    _ ______ _____  ____  
-     | | |  | |  ____/ ____|/ __ \ 
+      _ _    _ ______ _____  ____
+     | | |  | |  ____/ ____|/ __ \
      | | |  | | |__ | |  __| |  | |
  _   | | |  | |  __|| | |_ | |  | |
 | |__| | |__| | |___| |__| | |__| |
- \____/ \____/|______\_____|\____/ 
+ \____/ \____/|______\_____|\____/
 
 */
 
@@ -71,10 +84,10 @@ function imprimir_paridad($paridad){
 if (!isset($_SESSION['puntuacion'])) {
     $_SESSION['puntuacion'] = 10;
     $_SESSION['jugar'] = true;
-} 
+}
 elseif ($_SESSION['puntuacion'] >= 20 || $_SESSION['puntuacion'] <= 0) {
     $_SESSION['jugar'] = false;
-} 
+}
 else {
     $_SESSION['jugar'] = true;
 }
@@ -82,8 +95,17 @@ else {
 // Si se pulsa el boton reiniciar reinicia el juego entero
 if (isset($_POST['reiniciar'])) {
     $_SESSION['puntuacion'] = 10;
-    $_POST['valor_usuario'] = null; 
-    $_SESSION['jugar'] = true; 
+    $_SESSION['jugar'] = true;
+    $valor_usuario = null;
+    $dado1 = null;
+    $dado2 = null;
+    $suma = null;
+    $mensaje_comprobar = null;
+    $eleccion = null;
+    $coger_paridad = null;
+    $var_paridad = null;
+    $error = null;
+    $reiniciar_juego = "Pulsa el botón reiniciar para jugar de nuevo.";
 }
 
 // Si la varialbe de sesion jugar es true entonces se puede jugar
@@ -91,17 +113,15 @@ if ($_SESSION["jugar"]) {
 
     [$suma, $dado1, $dado2]= tirar();
     $paridad = calcular_par($suma);
-    
+
     // Comprueba si se ha seleccionado paridad e introducido un numero, si es el caso, sale un error.
     if (isset($_POST['submit'])) {
         if (!empty($_POST["coger_paridad"]) && !empty($_POST["valor_usuario"])) {
-            $_POST["valor_usuario"] = "";
-            $_POST["coger_paridad"] = "";
             $error = "No se pueden elegir ambas opciones a la vez.";
-            $comprobar=false;
+            $comprobar_juego=false;
         }
         else {
-            $comprobar=true;
+            $comprobar_juego=true;
         }
     }
 
@@ -109,7 +129,7 @@ if ($_SESSION["jugar"]) {
         Si tiene algun valor, comprueba que sea numerico, si no es numerico
         Luego comprueba que ese número sea enteros entre 2 y 12
     */
-    if ($comprobar){
+    if ($comprobar_juego){
         if (isset($_POST["valor_usuario"])){            // Comprueba que hay input del usuario
             if (!is_numeric(htmlspecialchars($_POST["valor_usuario"]))) { // Comprueba que el valor es un número
                 $valor_usuario = "No has introducido un número.";
@@ -142,12 +162,12 @@ if ($_SESSION["jugar"]) {
             $coger_paridad = $_POST["coger_paridad"];
             $eleccion = "Has elegido: ";
 
-            $comprobar = imprimir_paridad($paridad);
+            $mensaje_comprobar = imprimir_paridad($paridad);
             /*
-               Suma 1 punto a la puntuacion 
-               si la funcion de comprobar la paridad
+               Suma 1 punto a la puntuacion
+               si la funcion de $mensaje_comprobar la paridad
                es igual a lo que haya escogido el usuario,
-               sino resta 1 punto. 
+               sino resta 1 punto.
             */
                     if ($coger_paridad == "Par" && $paridad){
                         $var_paridad = "Has ganado 1 punto.";
@@ -163,10 +183,10 @@ if ($_SESSION["jugar"]) {
                     }
         }
         else {
-            $comprobar = "No has escogido que sea par o impar";
+            $mensaje_comprobar = "No has escogido que sea par o impar";
         }
     }
-    
+
 }
 else {
     $coger_paridad = null;
@@ -176,11 +196,32 @@ else {
     $dado1=null;
     $dado2=null;
     $suma=null;
+    $error=null;
+    $mensaje_comprobar=null;
     $reiniciar_juego = "Pulsa el botón reiniciar para jugar de nuevo.";
 }
+/*
+Mete todas las variables en variables de sesion con la clave "vista"
+para que cuando se haga el "header" no se pierda la información.
+*/
+    $_SESSION['vista'] = [
+    'valor_usuario' => $valor_usuario,
+    'dado1' => $dado1,
+    'dado2' => $dado2,
+    'suma' => $suma,
+    'mensaje_comprobar' => $mensaje_comprobar,
+    'eleccion' => $eleccion,
+    'paridad' => $coger_paridad,
+    'var_paridad' => $var_paridad,
+    'reiniciar_juego' => $reiniciar_juego,
+    'error' => $error
+    ];
 
-
-
+    if (!$error){
+        header('Location: '.$_SERVER['PHP_SELF']);
+        exit();
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -205,7 +246,7 @@ else {
                 echo "<br>";
                 echo "$error";
                 echo "<br>";
-                echo "$comprobar";
+                echo "$mensaje_comprobar";
                 echo "<br>";
                 echo "$eleccion"."$coger_paridad";
                 echo "<br>";
@@ -229,23 +270,5 @@ else {
                 echo "<br>";
                 echo "$reiniciar_juego";
             ?>
-    <?php else: 
-            if ($_SERVER["REQUEST_METHOD"] == "GET") {
-                $_SESSION['puntuacion'] = 10;
-            }
-    ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Juego de dados</title>
-    <link rel="stylesheet" href="../estilo/examen_php.css">
-</head>
-<body>
-    <form action="<?= (htmlspecialchars($_SERVER["PHP_SELF"])); //El php se escribe en el propio documento.?>" method="post">
-    <h1>Examen</h1>
-        <p>Objetivo: <input type="text" name="valor_usuario" placeholder="Número"> <input type="submit" name="submit" value="Enviar"></p>
-        <p>Par: <input type="radio" name="coger_paridad" value="Par"> Impar: <input type="radio" name="coger_paridad"  value="Impar"></p>
-        
-    <?php endif; //Final del if?>
 </body>
 </html>
